1def5ee3ff29202d1c5510759abb34800c0ba52a
zakzag09 <zakzag09@outlook.jp>
2019-08-04 09:11:11 +0000

    Finish editing-users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },format: { with: VALID_EMAIL_REGEX },uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6}
+  validates :password, presence: true, length: { minimum: 6}, allow_nil:true
   
   
   #渡された文字列のハッシュ値を返します。
