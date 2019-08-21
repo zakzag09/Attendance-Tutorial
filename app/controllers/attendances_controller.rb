648cc4d68386f31a8c6316f68fe6179bed4ce042
zakzag09 <zakzag09@outlook.jp>
2019-08-21 15:30:33 +0000

    Finish edit-attendances3

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,7 @@
 class AttendancesController < ApplicationController
   before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
+  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
 
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
@@ -47,5 +48,17 @@ class AttendancesController < ApplicationController
  #1ヶ月分の勤怠情報を扱います。
  def attendances_params
    params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
+ end
+ 
+ 
+ #beforeフィルター
+ 
+ #　管理権限者、または現在ログインしているユーザーを許可します。
+ def admin_or_correct_user
+   @user = User.find(params[:user_id]) if @user.blank?
+   unless current_user?(@user) || current_user.admin?
+     flash[:danger] = "編集権限がありません。"
+     redirect_to(root_url)
+   end
  end
 end
